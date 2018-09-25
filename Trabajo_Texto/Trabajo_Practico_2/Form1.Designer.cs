84fdd12c8ffec80251204d3cec6f463ce9e50f75
damnedDeamon <33160500+damnedDeamon@users.noreply.github.com>
2018-09-25 08:23:43 -0300

    Add files via upload
    Proyecto terminado

---
+++ Trabajo_Texto/Trabajo_Practico_2/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Trabajo_Practico_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnExaminar = new System.Windows.Forms.Button();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.txtCaracter = new System.Windows.Forms.TextBox();
+            this.txtEspacio = new System.Windows.Forms.TextBox();
+            this.txtTabulador = new System.Windows.Forms.TextBox();
+            this.txtLineas = new System.Windows.Forms.TextBox();
+            this.txtVocal = new System.Windows.Forms.TextBox();
+            this.txtConsonate = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.cboQuitar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(22, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(236, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnExaminar
+            // 
+            this.btnExaminar.Location = new System.Drawing.Point(286, 12);
+            this.btnExaminar.Name = "btnExaminar";
+            this.btnExaminar.Size = new System.Drawing.Size(137, 23);
+            this.btnExaminar.TabIndex = 1;
+            this.btnExaminar.Text = "Examinar";
+            this.btnExaminar.UseVisualStyleBackColor = true;
+            this.btnExaminar.Click += new System.EventHandler(this.btnExaminar_Click);
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(204, 244);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(100, 20);
+            this.txtPalabra.TabIndex = 3;
+            // 
+            // txtCaracter
+            // 
+            this.txtCaracter.Location = new System.Drawing.Point(204, 270);
+            this.txtCaracter.Name = "txtCaracter";
+            this.txtCaracter.Size = new System.Drawing.Size(100, 20);
+            this.txtCaracter.TabIndex = 4;
+            // 
+            // txtEspacio
+            // 
+            this.txtEspacio.Location = new System.Drawing.Point(204, 296);
+            this.txtEspacio.Name = "txtEspacio";
+            this.txtEspacio.Size = new System.Drawing.Size(100, 20);
+            this.txtEspacio.TabIndex = 5;
+            // 
+            // txtTabulador
+            // 
+            this.txtTabulador.Location = new System.Drawing.Point(204, 322);
+            this.txtTabulador.Name = "txtTabulador";
+            this.txtTabulador.Size = new System.Drawing.Size(100, 20);
+            this.txtTabulador.TabIndex = 6;
+            // 
+            // txtLineas
+            // 
+            this.txtLineas.Location = new System.Drawing.Point(204, 348);
+            this.txtLineas.Name = "txtLineas";
+            this.txtLineas.Size = new System.Drawing.Size(100, 20);
+            this.txtLineas.TabIndex = 7;
+            // 
+            // txtVocal
+            // 
+            this.txtVocal.Location = new System.Drawing.Point(204, 374);
+            this.txtVocal.Name = "txtVocal";
+            this.txtVocal.Size = new System.Drawing.Size(100, 20);
+            this.txtVocal.TabIndex = 8;
+            // 
+            // txtConsonate
+            // 
+            this.txtConsonate.Location = new System.Drawing.Point(204, 400);
+            this.txtConsonate.Name = "txtConsonate";
+            this.txtConsonate.Size = new System.Drawing.Size(100, 20);
+            this.txtConsonate.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.btnProcesar);
+            this.groupBox1.Controls.Add(this.cboQuitar);
+            this.groupBox1.Location = new System.Drawing.Point(13, 426);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(411, 71);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Guardar Texto";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Quitar:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(310, 24);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(90, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(219, 24);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(85, 23);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // cboQuitar
+            // 
+            this.cboQuitar.FormattingEnabled = true;
+            this.cboQuitar.Items.AddRange(new object[] {
+            "Espacios",
+            "Vocales",
+            "Consonantes"});
+            this.cboQuitar.Location = new System.Drawing.Point(64, 26);
+            this.cboQuitar.Name = "cboQuitar";
+            this.cboQuitar.Size = new System.Drawing.Size(121, 21);
+            this.cboQuitar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Tag = "";
+            this.label1.Text = "Palabras:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Caracteres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Espacios en blanco:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Tabuladores:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(157, 351);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Lineas:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Vocales:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(126, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Consonantes:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(22, 54);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(401, 171);
+            this.txtTexto.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 510);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtConsonate);
+            this.Controls.Add(this.txtVocal);
+            this.Controls.Add(this.txtLineas);
+            this.Controls.Add(this.txtTabulador);
+            this.Controls.Add(this.txtEspacio);
+            this.Controls.Add(this.txtCaracter);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.btnExaminar);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnExaminar;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.TextBox txtCaracter;
+        private System.Windows.Forms.TextBox txtEspacio;
+        private System.Windows.Forms.TextBox txtTabulador;
+        private System.Windows.Forms.TextBox txtLineas;
+        private System.Windows.Forms.TextBox txtVocal;
+        private System.Windows.Forms.TextBox txtConsonate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ComboBox cboQuitar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTexto;
+    }
+}
+
---
+++ Trabajo_Texto/Trabajo_Practico_2/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_Practico_2
+{
+    public partial class Form1 : Form
+    {
+        private int linea;
+        private int vocal;
+        private int contCons;
+        private int contarPa;
+        private int tabulacion;
+        private int espacio;
+        private int carac;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExaminar_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog buscar = new OpenFileDialog();
+            buscar.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (buscar.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+
+                txtUrl.Text = buscar.FileName;
+                txtTexto.Text = System.IO.File.ReadAllText("" + txtUrl.Text);
+                txtTexto.Enabled = false;
+                
+                contarLinea();
+                contarPalabra();
+                contarEspacio();
+                contarCaracter();
+                contarTabulacion();
+                contarVocal();
+                contarConsonantes();
+            }
+
+
+        }
+
+        private void contarLinea()
+        {
+            linea = 0;
+
+            for (int i = 0; i < txtTexto.Lines.Count(); i++)
+            {
+                if (i > -1)
+                {
+                    linea += 1;
+                }
+            }
+            txtLineas.Text = linea.ToString();
+        }
+
+        private void contarEspacio()
+        {
+            String texto = txtTexto.Text.ToLower();
+            espacio = 0;
+            for (int i = 0; i < texto.Length; i++)
+            {
+                switch (texto[i])
+                {
+                    case ' ':
+                        espacio += 1;
+                        break;
+
+                        //case '  ':
+                        //    tab += 1;
+                        //    break;
+                }
+            }
+
+            txtEspacio.Text = espacio.ToString();
+
+        }
+
+        private void contarCaracter()
+        {
+            String[] caracter = new string[txtTexto.Text.Length];
+            carac = 0;
+            for (int i = 0; i < caracter.Length; i++)
+            {
+                carac += 1;
+
+            }
+            for (int i = 1; i < linea; i++)
+            {
+                carac -= 2;
+            }
+
+
+
+            txtCaracter.Text = carac.ToString();
+        }
+        private void contarTabulacion()
+        {
+            String tabs = txtTexto.Text.ToLower();
+            tabulacion = 0;
+            for (int i = 0; i < tabs.Length; i++)
+            {
+                switch (tabs[i])
+                {
+                    case '\t':
+                        tabulacion += 1;
+                        break;
+                }
+            }
+            txtTabulador.Text = tabulacion.ToString();
+
+        }
+        private void contarConsonantes()
+        {
+            String consonante = txtTexto.Text.ToLower();
+            contCons = 0;
+
+            for (int i = 0; i < consonante.Length; i++)
+            {
+                switch (consonante[i])
+                {
+                    case 'b':
+                        contCons += 1;
+                        break;
+                    case 'c':
+                        contCons += 1;
+                        break;
+                    case 'd':
+                        contCons += 1;
+                        break;
+                    case 'f':
+                        contCons += 1;
+                        break;
+                    case 'g':
+                        contCons += 1;
+                        break;
+                    case 'h':
+                        contCons += 1;
+                        break;
+                    case 'j':
+                        contCons += 1;
+                        break;
+                    case 'k':
+                        contCons += 1;
+                        break;
+                    case 'l':
+                        contCons += 1;
+                        break;
+                    case 'm':
+                        contCons += 1;
+                        break;
+                    case 'n':
+                        contCons += 1;
+                        break;
+                    case 'p':
+                        contCons += 1;
+                        break;
+                    case 'q':
+                        contCons += 1;
+                        break;
+                    case 'r':
+                        contCons += 1;
+                        break;
+                    case 's':
+                        contCons += 1;
+                        break;
+                    case 't':
+                        contCons += 1;
+                        break;
+                    case 'v':
+                        contCons += 1;
+                        break;
+                    case 'w':
+                        contCons += 1;
+                        break;
+                    case 'x':
+                        contCons += 1;
+                        break;
+                    case 'y':
+                        contCons += 1;
+                        break;
+                    case 'z':
+                        contCons += 1;
+                        break;
+                }
+            }
+
+            txtConsonate.Text = contCons.ToString();
+        }
+        private void contarVocal()
+        {
+            String vocales = txtTexto.Text.ToLower();
+            vocal = 0;
+
+            for (int i = 0; i < vocales.Length; i++)
+            {
+                switch (vocales[i])
+                {
+                    case 'a':
+                        vocal += 1;
+                        break;
+                    case 'e':
+                        vocal += 1;
+                        break;
+                    case 'i':
+                        vocal += 1;
+                        break;
+                    case 'o':
+                        vocal += 1;
+                        break;
+                    case 'u':
+                        vocal += 1;
+                        break;
+                }
+            }
+            txtVocal.Text = vocal.ToString();
+        }
+        private void contarPalabra()
+        {
+            var palabra = txtTexto.Text.Split().GroupBy(x => x);
+            contarPa = 0;
+            foreach (var pal in palabra)
+            {
+                if (pal != null)
+                {
+                    contarPa += 1;
+
+                }
+
+            }
+            if (linea >= 2)
+            {
+                contarPa -= 1;
+            }
+            txtPalabra.Text = contarPa.ToString();
+        }
+
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            if (cboQuitar.SelectedIndex == 0)
+            {
+                txtTexto.Text =  txtTexto.Text.Replace(" ","");
+                txtTexto.Text = txtTexto.Text.Replace("\t", "");
+                txtEspacio.Text = "0";
+
+            }else if (cboQuitar.SelectedIndex == 1)
+            {
+                txtTexto.Text = txtTexto.Text.Replace("a", "");
+                txtTexto.Text = txtTexto.Text.Replace("e", "");
+                txtTexto.Text = txtTexto.Text.Replace("i", "");
+                txtTexto.Text = txtTexto.Text.Replace("o", "");
+                txtTexto.Text = txtTexto.Text.Replace("u", "");
+                txtVocal.Text = "0";
+
+            } else if (cboQuitar.SelectedIndex == 2)
+            {
+                txtTexto.Text = txtTexto.Text.Replace("b", "");
+                txtTexto.Text = txtTexto.Text.Replace("c", "");
+                txtTexto.Text = txtTexto.Text.Replace("d", "");
+                txtTexto.Text = txtTexto.Text.Replace("f", "");
+                txtTexto.Text = txtTexto.Text.Replace("g", "");
+                txtTexto.Text = txtTexto.Text.Replace("h", "");
+                txtTexto.Text = txtTexto.Text.Replace("i", "");
+                txtTexto.Text = txtTexto.Text.Replace("j", "");
+                txtTexto.Text = txtTexto.Text.Replace("k", "");
+                txtTexto.Text = txtTexto.Text.Replace("l", "");
+                txtTexto.Text = txtTexto.Text.Replace("m", "");
+                txtTexto.Text = txtTexto.Text.Replace("n", "");
+                txtTexto.Text = txtTexto.Text.Replace("p", "");
+                txtTexto.Text = txtTexto.Text.Replace("q", "");
+                txtTexto.Text = txtTexto.Text.Replace("r", "");
+                txtTexto.Text = txtTexto.Text.Replace("s", "");
+                txtTexto.Text = txtTexto.Text.Replace("t", "");
+                txtTexto.Text = txtTexto.Text.Replace("v", "");
+                txtTexto.Text = txtTexto.Text.Replace("w", "");
+                txtTexto.Text = txtTexto.Text.Replace("x", "");
+                txtTexto.Text = txtTexto.Text.Replace("y", "");
+                txtTexto.Text = txtTexto.Text.Replace("z", "");
+
+                txtTexto.Text = txtTexto.Text.Replace("B", "");
+                txtTexto.Text = txtTexto.Text.Replace("C", "");
+                txtTexto.Text = txtTexto.Text.Replace("D", "");
+                txtTexto.Text = txtTexto.Text.Replace("F", "");
+                txtTexto.Text = txtTexto.Text.Replace("G", "");
+                txtTexto.Text = txtTexto.Text.Replace("H", "");
+                txtTexto.Text = txtTexto.Text.Replace("I", "");
+                txtTexto.Text = txtTexto.Text.Replace("J", "");
+                txtTexto.Text = txtTexto.Text.Replace("K", "");
+                txtTexto.Text = txtTexto.Text.Replace("L", "");
+                txtTexto.Text = txtTexto.Text.Replace("M", "");
+                txtTexto.Text = txtTexto.Text.Replace("N", "");
+                txtTexto.Text = txtTexto.Text.Replace("P", "");
+                txtTexto.Text = txtTexto.Text.Replace("Q", "");
+                txtTexto.Text = txtTexto.Text.Replace("R", "");
+                txtTexto.Text = txtTexto.Text.Replace("S", "");
+                txtTexto.Text = txtTexto.Text.Replace("T", "");
+                txtTexto.Text = txtTexto.Text.Replace("V", "");
+                txtTexto.Text = txtTexto.Text.Replace("W", "");
+                txtTexto.Text = txtTexto.Text.Replace("X", "");
+                txtTexto.Text = txtTexto.Text.Replace("Y", "");
+                txtTexto.Text = txtTexto.Text.Replace("Z", "");
+
+                txtConsonate.Text = "0";
+
+            }
+
+            contarLinea();
+            contarPalabra();
+            contarEspacio();
+            contarCaracter();
+            contarTabulacion();
+            contarVocal();
+            contarConsonantes();
+
+            if (txtConsonate.Text == "0" && txtVocal.Text == "0" && txtEspacio.Text == "0")
+            {
+                txtLineas.Text = "0";
+            }
+            if (txtVocal.Text == "0" || txtConsonate.Text == "0")
+            {
+                txtPalabra.Text = "0";
+            }
+            
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog guardar = new SaveFileDialog();
+            guardar.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (guardar.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter guardarTexto = File.AppendText(guardar.FileName)) 
+                guardarTexto.WriteLine(txtTexto.Text);
+            }
+        }
+    }
+}
